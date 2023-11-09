f172bb141728c27d20f8aa8a50314c295924c9c6
Florian Königsmann <70534154+Zwergenland27@users.noreply.github.com>
2023-11-09 15:07:14 +0100

    current test data

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -3,57 +3,71 @@ using CleanValidationMapper;
 using CleanValidationMapper.Errors;
 using CleanValidationMapper.RequestValidation;
 
-var body = new Body("1", "3");
-body.WithWurzel(null, "Test");
-body.Validated("Test");
+var body = new Body("1", "2", "3", "4", null);
 var res = body.Validate();
 Console.WriteLine("Tst");
 public class Body : AbstractBody<Test>
 {
     private RequiredReferenceProperty<CustomParam> customParamBuilder;
-    public Body(string? param, string? param3)
+    public Body(string? param, string? param2, string? param3, string? param4, string? param5)
     {
-        _propertyBuilder.MapRequired(t => t.Param2, pb =>
+        Param = param;
+        Param2 = param2;
+        Param3 = param3;
+        Param4 = param4;
+        Param5 = param5;
+    }
+
+    public string? Param { get; }
+    public string? Param2 { get; }
+    public string? Param3 { get; }
+    public string? Param4 { get; }
+    public string? Param5 { get; }
+
+    protected override void Configure(RequiredReferenceProperty<Test> propertyBuilder)
+    {
+        propertyBuilder.MapRequired(t => t.Param2, pb =>
+        {
+            pb.MapRequired(cp => cp.Titel)
+               .FromParameter(Param3, Error.Validation("Titel missing", ""));
+            pb.MapRequired(cp => cp.Value, cb =>
             {
-                pb.MapRequired(cp => cp.Titel)
-                   .FromParameter(param3, Error.Validation("Titel missing", ""));
-                customParamBuilder = pb;
+                cb.MapRequired(Wurzel => Wurzel.Wert)
+                   .FromParameter(Param2, Error.Validation("1 fehlt", ""));
+
+                cb.MapRequired(Wurzel => Wurzel.Test)
+                    .MapRequired(Primitiv => Primitiv.Test)
+                    .FromParameter(Param4, Error.Validation("Primitiv fehlt", ""));
             });
-
-        _propertyBuilder.MapRequired(t => t.Param)
-            .FromParameter(param, Error.Validation("Parameter missing", ""));
-    }
-
-    public void WithWurzel(string? param2, string? param4)
-    {
-        customParamBuilder.MapOptional(cp => cp.Value, cb =>
-        {
-            cb.MapRequired(Wurzel => Wurzel.Wert)
-               .FromParameter(param2, Error.Validation("1 fehlt", ""));
-
-            cb.MapOptional(Wurzel => Wurzel.Test)
-                .MapRequired(Primitiv => Primitiv.Test)
-                .FromParameter(param4, Error.Validation("Primitiv fehlt", ""));
-        });
-    }
-
-    public void Validated(string? parameter)
-    {
-        _propertyBuilder.MapRequired(t => t.Validated, vb =>
-        {
-            vb.MapRequiredParameter<string>("var1")
-                .FromParameter(parameter, Error.Validation("validated missing", ""));
-            vb.ByCalling(ValidatedParam.Create);
         });
 
+        propertyBuilder.MapRequired(t => t.Param)
+            .FromParameter(Param, Error.Validation("Parameter missing", ""));
+
+        propertyBuilder.MapOptional (t => t.Validated)
+            .ByCalling(ValidatedParam.Create, methodParameters =>
+            {
+                methodParameters.AddRequired<string>().FromParameter(Param5, Error.Validation("Paramter 5 missing", ""));
+            });
     }
+
+    //public void Validated(string? parameter)
+    //{
+    //    _propertyBuilder.MapRequired(t => t.Validated, vb =>
+    //    {
+    //        vb.MapRequiredParameter<string>("var1")
+    //            .FromParameter(parameter, Error.Validation("validated missing", ""));
+    //        vb.ByCalling(ValidatedParam.Create);
+    //    });
+
+    //}
 }
 
 public record Primitiv(string Test);
 
-public record Wurzel(string Wert, Primitiv? Test); 
+public record Wurzel(string Wert, Primitiv Test); 
 
-public record CustomParam(Wurzel? Value, string Titel);
+public record CustomParam(Wurzel Value, string Titel);
 
 public record ValidatedParam
 {
@@ -70,10 +84,11 @@ public record ValidatedParam
     }
     public static CanFail<ValidatedParam> Create(string var1)
     {
+        CanFail<ValidatedParam> result = new();
         if (var1 == "fail") return Error.Validation("ValidatedParam.Fail", "");
 
-        return new ValidatedParam(var1);
+        return result.SetValue(new ValidatedParam(var1));
     }
 }
 
-public record Test(string Param, CustomParam Param2, ValidatedParam Validated);
+public record Test(string Param, CustomParam Param2, ValidatedParam? Validated);
